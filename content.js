eaa51ec4103dda313fec929afa89fd134ab35b2b
Dev Vibhu Someshwar <contact@devsdigest.com>
2019-08-08 00:28:33 -0400

    Refactored due to Chrome API restrictions

--- content.js
+++ content.js
@@ -1,9 +1,36 @@
-import memecase from './cases.js';
+// Memecase transform methods
+
+function spongecase(text) {
+  let outtxt = "";
+  for (let i=0; i<text.length; i++) {
+    i%2 === 0 ? outtxt += text[i].toLowerCase() : outtxt += text[i].toUpperCase();
+  }
+  return outtxt;
+}
+
+let memecase = {
+  spongecase: spongecase
+};
 
 let currentCase = memecase.spongecase;
+let active = false;
 
-document.querySelectorAll("input[type=text]").forEach(function(val, ind, listObj) {
-  val.addEventListener('input', function(e) {
-    e.target.value = currentCase(e.target.value);
-  });
+function onInputChange(e) {
+  e.target.value = currentCase(e.target.value);
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  active = !active;
+  if (request.toggle == true) {
+    if (active) {
+      document.querySelectorAll("input[type=text], textarea").forEach(function(val, ind, listObj) {
+        val.addEventListener('input', onInputChange);
+      });
+    }
+    else {
+      document.querySelectorAll("input[type=text]. textarea").forEach(function(val, ind, listObj) {
+        val.removeEventListener('input', onInputChange);
+      });
+    }
+  }
 });
